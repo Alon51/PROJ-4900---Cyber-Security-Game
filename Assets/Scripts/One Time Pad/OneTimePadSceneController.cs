6b4a22a102029fb4f2388434724dd25de4452e23
Alon <Alon51B@gmail.com>
2018-07-25 15:34:02 -0400

    Made changes to month 10
    Added information to month 10.
    The dialog functioning but still need modifications in the questions and answers.
    
    Working next on the illustration of the one time pad encryption.

---
+++ Assets/Scripts/One Time Pad/OneTimePadSceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneTimePadSceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        // displays opening text
+        GameObject.Find("dlg_one_time_pad").GetComponent<DialogueTrigger>().TriggerDialogue();
+
+        // glitch animation
+        GameObject.FindObjectOfType<GlitchCamera>().StartGlitch();
+	}
+}
